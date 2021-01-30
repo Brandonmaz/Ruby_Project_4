04b0098e923408fd00e1924d3edbae8921c42745
James Torres <james.torres01@gmail.com>
2021-01-30 15:18:24 -0500

    show route working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,12 +4,7 @@ class EntriesController < ApplicationController
     end
 
     def show
-<<<<<<< HEAD
-        render json: Entries.find(params[:id])
+        render json: Entry.find(params[:id])
     end
     
-=======
-        render json: Entry.find(params["id"])
-    end
->>>>>>> 0009ffd2a474d0589af23a3f0b5294466d241e6d
 end
