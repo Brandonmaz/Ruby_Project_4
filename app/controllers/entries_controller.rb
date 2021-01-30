7b403169e6f1763e998aa724b46bbfb249164f1a
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-01-30 16:28:20 -0500

    create route completed

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,14 @@
 class EntriesController < ApplicationController
+    # skip_before_action :verify_authenticity_token
     def index
         render json: Entry.all
     end
 
     def show
         render json: Entry.find(params["id"])
+    end
+
+    def create
+        render json: Entry.create(params["entry"])
     end
 end
