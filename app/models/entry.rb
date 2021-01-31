a6270723080967906303ad7b86c4feed66501400
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-01-30 19:10:19 -0500

    heroku

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,9 @@
 class Entry < ApplicationRecord
-    if ENV["DATABASE_URL"]
-        PG.connect(ENV['DATABASE_URL'])
-    elsif
-        DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'to_do_api_development'})
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'to_do_api_development')
     end
 
     def self.all
