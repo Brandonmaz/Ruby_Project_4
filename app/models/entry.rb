db0fee8fa633c074eb093ebb0aab323bfbd8fd92
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-01-31 18:11:20 -0500

    testing seeding data on heroku

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -23,21 +23,21 @@ class Entry < ApplicationRecord
     #     }
     # end
 
-    def self.create(opts)
-        results = DB.exec(
-            <<-SQL
-                INSERT INTO entries (task, description, due_date, done)
-                VALUES ('#{opts["task"]}', '#{opts["description"]}', '#{opts["due_date"]}', #{opts["done"]})
-                RETURNING task, description, due_date, done;
-            SQL
-        )
-        return {
-            "task" => results.first["task"],
-            "description" => results.first["description"],
-            "due_date" => results.first["due_date"],
-            "done" => results.first["done"]
-        }
-    end
+    # def self.create(opts)
+    #     results = DB.exec(
+    #         <<-SQL
+    #             INSERT INTO entries (task, description, due_date, done)
+    #             VALUES ('#{opts["task"]}', '#{opts["description"]}', '#{opts["due_date"]}', #{opts["done"]})
+    #             RETURNING task, description, due_date, done;
+    #         SQL
+    #     )
+    #     return {
+    #         "task" => results.first["task"],
+    #         "description" => results.first["description"],
+    #         "due_date" => results.first["due_date"],
+    #         "done" => results.first["done"]
+    #     }
+    # end
 
     # def self.delete(id)
     #     results = DB.exec("DELETE FROM entries WHERE id=#{id};")
