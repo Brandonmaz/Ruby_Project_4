d9950d49912ee90529d38593a04cc8d56c833104
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-01-31 18:04:26 -0500

    api test

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -13,15 +13,15 @@ class Entry < ApplicationRecord
         end
     end
 
-    def self.find(id)
-        results = DB.exec("SELECT * FROM entries WHERE id=#{id};")
-        return {
-            "task" => results.first["task"],
-            "description" => results.first["description"],
-            "due_date" => results.first["due_date"],
-            "done" => results.first["done"]
-        }
-    end
+    # def self.find(id)
+    #     results = DB.exec("SELECT * FROM entries WHERE id=#{id};")
+    #     return {
+    #         "task" => results.first["task"],
+    #         "description" => results.first["description"],
+    #         "due_date" => results.first["due_date"],
+    #         "done" => results.first["done"]
+    #     }
+    # end
 
     def self.create(opts)
         results = DB.exec(
@@ -39,30 +39,30 @@ class Entry < ApplicationRecord
         }
     end
 
-    def self.delete(id)
-        results = DB.exec("DELETE FROM entries WHERE id=#{id};")
-        return { "deleted": true }
-    end
+    # def self.delete(id)
+    #     results = DB.exec("DELETE FROM entries WHERE id=#{id};")
+    #     return { "deleted": true }
+    # end
 
-    def self.update(id, opts)
-        results = DB.exec(
-            <<-SQL
-                UPDATE entries
-                SET task = '#{opts["task"]}',
-                description = '#{opts["description"]}',
-                due_date = '#{opts["due_date"]}',
-                done = #{opts["done"]}
-                WHERE id = #{id}
-                RETURNING task, description, due_date, done;
-            SQL
-        )
-        return {
-            "task" => results.first["task"],
-            "description" => results.first["description"],
-            "due_date" => results.first["due_date"],
-            "done" => results.first["done"]
-        }
-    end
+    # def self.update(id, opts)
+    #     results = DB.exec(
+    #         <<-SQL
+    #             UPDATE entries
+    #             SET task = '#{opts["task"]}',
+    #             description = '#{opts["description"]}',
+    #             due_date = '#{opts["due_date"]}',
+    #             done = #{opts["done"]}
+    #             WHERE id = #{id}
+    #             RETURNING task, description, due_date, done;
+    #         SQL
+    #     )
+    #     return {
+    #         "task" => results.first["task"],
+    #         "description" => results.first["description"],
+    #         "due_date" => results.first["due_date"],
+    #         "done" => results.first["done"]
+    #     }
+    # end
 
 
 end
