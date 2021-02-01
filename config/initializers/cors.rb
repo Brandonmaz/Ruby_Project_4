ac9504f8e94437ce19cb340353e511315e61ba9e
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-02-01 12:43:38 -0500

    changed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://todolistabcj.herokuapp.com'
+    origins 'https://abcjtodolist.herokuapp.com/'
+
 
     resource '*',
       headers: :any,
